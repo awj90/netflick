ccdd1cedabd3b56234ef549da02dea102060a824
awj90 <dennis.awj@gmail.com>
2023-07-20 17:24:14 +0800

    clean up config and environments

--- client/src/app/configs/app-config.ts
+++ client/src/app/configs/app-config.ts
@@ -3,8 +3,6 @@ export default {
     issuer: 'https://dev-50458755.okta.com/oauth2/default',
     clientId: '0oaa0qs617MRs7gfl5d7',
     redirectUri: window.location.origin + '/login/callback',
-    // redirectUri: 'https://netflick-store.netlify.app/login/callback',
-    // redirectUri: 'http://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
   },
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   serverApiUrl: 'https://netflick-production.up.railway.app/api',
   stripePublishableKey:
     'pk_test_51NLhrtLRBUwzTelQQBRisRwsk83v7RZtoEVrMubGsaf1TNOQyxEQphC6cP78dGGapPoQ7bn9fg0lWvh3gJ1DoNs000KGDam6QS',
